b7ca08b0662eb7a329ab26b2351125f6849d7084
william davis <williamjsdavis@berkeley.edu>
2020-04-01 23:07:09 -0700

    Added contour plot, make mutable array into a macro?

--- main.jl
+++ main.jl
@@ -34,6 +34,9 @@ function main(tArray, xArray, D)
     dx = 1/(xArray.len-1);
     dt = 0.5*dx*dx/D;
     s = D*dt/(dx*dx);
+    function stepTime(p, s, nx)
+        p[2:end-1] = s.*p[3:nx]-(2*s-1).*p[2:nx-1]+s.*p[1:nx-2];
+    end
 
     YplotObject = smoothLivePlotY(xArray, p0, myPlot)
     #YplotObject = @makeSmoothLivePlot myPlot(xArray, p0)
@@ -43,15 +46,8 @@ function main(tArray, xArray, D)
     end
 end
 function main2(tArray, xArray, D)
-
     p0 = getInitialDist(xArray)
     pMax = maximum(p0)
-    p = copy(p0)
-
-    dx = 1/(xArray.len-1);
-    dt = 0.5*dx*dx/D;
-    s = D*dt/(dx*dx);
-
     XplotObject = smoothLivePlotX(xArray, p0, myPlot)
     #YplotObject = @makeSmoothLivePlot myPlot(xArray, p0)
     for tt in tArray
@@ -59,30 +55,54 @@ function main2(tArray, xArray, D)
     end
 end
 function main3(tArray, xArray, D)
-
     p0 = getInitialDist(xArray)
     pMax = maximum(p0)
-    p = copy(p0)
-
-    dx = 1/(xArray.len-1);
-    dt = 0.5*dx*dx/D;
-    s = D*dt/(dx*dx);
-
     XYplotObject = smoothLivePlotXY(xArray, p0, myPlot)
     #YplotObject = @makeSmoothLivePlot myPlot(xArray, p0)
     for tt in tArray
         XYplotObject[] = [XYplotObject[][1]*1.01, XYplotObject[][2]*0.99]
     end
-    return XYplotObject
+end
+function main4(tArray, xArray, D)
+    x = 1:0.05:20
+    y = 1:0.05:10
+    f(x, y, t) = begin
+        (3x + y^2) * abs(sin(x) + (1-t)*cos(y))
+    end
+    X = repeat(reshape(x, 1, :), length(y), 1)
+    Y = repeat(y, 1, length(x))
+    Z = map((x, y) -> f(x, y, 0.0), X, Y)
+    ZplotObject = smoothLivePlotZ(x, y, Z, myPlotZ)
+    #YplotObject = @makeSmoothLivePlot myPlot(xArray, p0)
+    ttt = 0.0:0.1:1.0
+    for tt in ttt
+        Z = map((x, y) -> f(x, y, tt), X, Y)
+        ZplotObject[] = [Z]
+    end
+end
+function main5(tArray, xArray, D)
+    p0 = getInitialDist(xArray)
+    pMax = maximum(p0)
+    p = copy(p0)
+    titleText = "Title, step: "
+
+    XtextPlotObject = smoothLivePlotXtext(myPlotTitle, [xArray, p0, titleText], (true, false, true))
+    #YplotObject = @makeSmoothLivePlot myPlot(xArray, p0)
+    for tt in 1:length(tArray)
+        #XtextPlotObject[] = [XtextPlotObject[][1]*0.99, string(titleText, tt)]
+        #XtextPlotObject[] = [XtextPlotObject[][1]*0.99, XtextPlotObject[][2], string(titleText, tt)]
+        replaceMutablePlotElement!(XtextPlotObject, 3, string(titleText, tt))
+    end
+    return XtextPlotObject
+end
+function replaceMutablePlotElement!(MutablePlotArray, ind, agr3)
+    MutablePlotArray[] = [MutablePlotArray[][1], MutablePlotArray[][2], agr3] # Make into a macro?
 end
 function getInitialDist(xArray)
     x0 = 0.0
     sig = 2.0
     f(x) = exp(-((x - x0)/sig)^2)
     p = f.(xArray)
-end
-function stepTime(p, s, nx)
-    p[2:end-1] = s.*p[3:nx]-(2*s-1).*p[2:nx-1]+s.*p[1:nx-2];
 end
 function myPlot(xx, yy)
     plot(xx, yy, label = "", color = :red)
@@ -93,5 +113,21 @@ function myPlot(xx, yy)
     xlabel!("X label")
     ylabel!("Y label")
 end
+function myPlotTitle(xx, yy, titleText)
+    sleep(0.0001)
+    plot(xx, yy, label = "", color = :red)
+    scatter!(xx, yy, label = "")
+    xlims!(-5, 5)
+    ylims!(0, 1)
+    title!(titleText)
+    xlabel!("X label")
+    ylabel!("Y label")
+end
+function myPlotZ(xx, yy, ZZ)
+    p1 = contour(xx, yy, ZZ, fill = true, clims=(0,300));
+    title!("Title")
+    xlabel!("X label")
+    ylabel!("Y label")
+end
 
-testObj = main3(tArray, xArray, D)
+main5(tArray, xArray, D)
--- src/SmoothLivePlot.jl
+++ src/SmoothLivePlot.jl
@@ -1,19 +1,17 @@
 ## Smooth live plot
 using Observables, WebIO
 
-
-function delayPlot(x, y, plotFunction)
+function delayPlot2(plotFunction, inArgs...)
     sleep(0.0001)
-    plotFunction(x, y)
+    plotFunction(inArgs...)
 end
 function smoothLivePlotY(x, y, plotFunction)
     data_obs = Observable{Any}(y)
     plt_obs = Observable{Any}(plotFunction(x, y))
 
-    #map!(yy -> plot(x, yy), plt_obs, data_obs)
-    map!(yy -> delayPlot(x, yy, plotFunction), plt_obs, data_obs)
+    map!(yy -> delayPlot2(plotFunction, x, yy), plt_obs, data_obs)
 
-    ui = dom"div"( plt_obs );
+    ui = dom"div"(plt_obs);
     display(ui)
     sleep(0.4)
     return data_obs
@@ -22,10 +20,9 @@ function smoothLivePlotX(x, y, plotFunction)
     data_obs = Observable{Any}(x)
     plt_obs = Observable{Any}(plotFunction(x, y))
 
-    #map!(yy -> plot(x, yy), plt_obs, data_obs)
-    map!(xx -> delayPlot(xx, y, plotFunction), plt_obs, data_obs)
+    map!(xx -> delayPlot2(plotFunction, xx, y), plt_obs, data_obs)
 
-    ui = dom"div"( plt_obs );
+    ui = dom"div"(plt_obs);
     display(ui)
     sleep(0.4)
     return data_obs
@@ -34,10 +31,84 @@ function smoothLivePlotXY(x, y, plotFunction)
     data_obs = Observable{Any}([x, y])
     plt_obs = Observable{Any}(plotFunction([], []))
 
-    #map!(yy -> plot(x, yy), plt_obs, data_obs)
-    map!(inArg -> delayPlot(inArg..., myPlot), plt_obs, data_obs)
+    map!(inArg -> delayPlot2(plotFunction, inArg...), plt_obs, data_obs)
 
-    ui = dom"div"( plt_obs );
+    ui = dom"div"(plt_obs);
+    display(ui)
+    sleep(0.4)
+    return data_obs
+end
+function smoothLivePlotZ(x, y, z, plotFunction)
+    data_obs = Observable{Any}(z)
+    plt_obs = Observable{Any}(plotFunction(x, y, z))
+
+    map!(zz -> delayPlot2(plotFunction, x, y, zz), plt_obs, data_obs)
+
+    ui = dom"div"(plt_obs);
+    display(ui)
+    sleep(0.4)
+    return data_obs
+end
+mutable struct changableArguments
+    x
+    y
+    titleText
+end
+function smoothLivePlotXtext(plotFunction, inArgs, boolTuple)
+    x = inArgs[1]
+    y = inArgs[2]
+    titleText = inArgs[3]
+    mutableArgs = [boolTuple...]
+    immutableArgs = [.!boolTuple...]
+    mutableArray = inArgs[mutableArgs]
+
+    #data_obs = Observable{Any}(mutableArray)
+    data_obs = Observable{Any}(inArgs)
+    plt_obs = Observable{Any}(plotFunction(x, y, titleText))
+
+    function agrToMutableArgs(inArgs, mutableArgs, mapArgi)
+        outArgs = similar(inArgs)
+        j = 1
+        for ii in 1:length(inArgs)
+            if mutableArgs[ii]
+                outArgs[ii] = mapArgi[j]
+                j = j + 1
+            else
+                outArgs[ii] = inArgs[ii]
+            end
+        end
+        return outArgs
+    end
+
+    #map!(mapArg -> delayPlot2(plotFunction, mapArg[1], y, mapArg[2]), plt_obs, data_obs)
+    #map!(mapArg -> plotFunction(agrToMutableArgs(inArgs, mutableArgs, mapArg)...), plt_obs, data_obs)
+    map!(mapArg -> plotFunction(mapArg...), plt_obs, data_obs)
+
+    ui = dom"div"(plt_obs);
+    display(ui)
+    sleep(0.4)
+    return data_obs
+end
+function agrToMutableArgs2(inArgs, mutableArgs, mapArgi)
+    outArgs = similar(inArgs)
+    j = 1
+    for ii in 1:length(inArgs)
+        if mutableArgs[ii]
+            outArgs[ii] = mapArgi[j]
+            j = j + 1
+        else
+            outArgs[ii] = inArgs[ii]
+        end
+    end
+    return outArgs
+end
+function smoothLivePlotGeneral(plotFunction, plotArgs)
+    data_obs = Observable{Any}(z)
+    plt_obs = Observable{Any}(plotFunction(x, y, z))
+
+    map!(zz -> delayPlot2(plotFunction, x, y, zz), plt_obs, data_obs)
+
+    ui = dom"div"(plt_obs);
     display(ui)
     sleep(0.4)
     return data_obs
